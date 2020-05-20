7890c9d9defcc7c0343f2f5a32a75ff2e6431e02
arpangreat <arpanthegreat41@gmail.com>
2020-05-20 19:24:16 +0530

    ''

--- Data_Types/src/DataTypes.rs
+++ Data_Types/src/DataTypes.rs
@@ -9,37 +9,31 @@
  Arrays
 */
 
-// Rust is a statically typed language, which means that it must know the types of all variables at compile time, however, the compiler can usually infer what type we want to use based on the value and how we use it. 
+// Rust is a statically typed language, which means that it must know the types of all variables at compile time, however, the compiler can usually infer what type we want to use based on the value and how we use it.
 
 pub fn run() {
+    // Default is 'i32'
+    let x = 1;
 
-  
- // Default is 'i32'
- let x = 1;
+    // Default is 'f64'
+    let y = 2.5;
 
- // Default is 'f64'
- let y = 2.5;
+    // Add explicit type
+    let z: i64 = 25468456974354;
 
- // Add explicit type
- let z: i64 = 25468456974354;
+    // Find max size
+    println!("MAX i32: {}", std::i32::MAX);
+    println!("MAX i64: {}", std::i64::MAX);
 
- // Find max size
- println!("MAX i32: {}", std::i32::MAX);
- println!("MAX i64: {}", std::i64::MAX);
+    // Boolean
+    let is_active = true;
 
- // Boolean
- let is_active = true;
+    // Get boolean from expression
+    let is_greater = 10 < 5;
 
+    // Character
+    let a1 = 'a';
+    let face = '\u{1F468}';
 
- // Get boolean from expression
- let is_greater = 10 < 5;
-
- // Character
- let a1 = 'a';
- let face = '\u{1F468}';
-
- println!("{:?}",(x,y,z,is_active,is_greater,a1,face));
-
-
- 
+    println!("{:?}", (x, y, z, is_active, is_greater, a1, face));
 }
---
+++ Data_Types/target/debug/deps/Data_Types-b1e39618307ce96a.d
@@ -0,0 +1,10 @@
+/home/arpangreat/RustExs/Data_Types/target/debug/deps/Data_Types-b1e39618307ce96a.rmeta: src/main.rs src/DataTypes.rs src/String.rs src/Tuples.rs src/Arrays.rs src/Vectors.rs
+
+/home/arpangreat/RustExs/Data_Types/target/debug/deps/Data_Types-b1e39618307ce96a.d: src/main.rs src/DataTypes.rs src/String.rs src/Tuples.rs src/Arrays.rs src/Vectors.rs
+
+src/main.rs:
+src/DataTypes.rs:
+src/String.rs:
+src/Tuples.rs:
+src/Arrays.rs:
+src/Vectors.rs:
