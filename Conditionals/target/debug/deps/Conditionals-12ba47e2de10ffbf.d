5c962c0c62314db0fffdce43465e42ad6601c2fb
arpangreat <arpanthegreat41@gmail.com>
2020-07-05 04:30:47 +0530

    android

---
+++ Conditionals/target/debug/deps/Conditionals-12ba47e2de10ffbf.d
@@ -0,0 +1,6 @@
+/data/data/com.termux/files/home/RustExs/Conditionals/target/debug/deps/Conditionals-12ba47e2de10ffbf.rmeta: src/main.rs src/ConditionalsDemo.rs
+
+/data/data/com.termux/files/home/RustExs/Conditionals/target/debug/deps/Conditionals-12ba47e2de10ffbf.d: src/main.rs src/ConditionalsDemo.rs
+
+src/main.rs:
+src/ConditionalsDemo.rs:
---
+++ Data_Types/target/debug/deps/Data_Types-9fe77af2780df649.d
@@ -0,0 +1,10 @@
+/data/data/com.termux/files/home/RustExs/Data_Types/target/debug/deps/Data_Types-9fe77af2780df649: src/main.rs src/Arrays.rs src/DataTypes.rs src/String.rs src/Tuples.rs src/Vectors.rs
+
+/data/data/com.termux/files/home/RustExs/Data_Types/target/debug/deps/Data_Types-9fe77af2780df649.d: src/main.rs src/Arrays.rs src/DataTypes.rs src/String.rs src/Tuples.rs src/Vectors.rs
+
+src/main.rs:
+src/Arrays.rs:
+src/DataTypes.rs:
+src/String.rs:
+src/Tuples.rs:
+src/Vectors.rs:
