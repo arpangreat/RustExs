61e7ad858221bd8e17bbe2c6e9a0697b6bba63bf
arpangreat <arpanthegreat41@gmail.com>
2020-07-15 11:00:04 +0530

    Mobile Commit

---
+++ MyDocTutorial/farenheit_celcius/target/debug/deps/farenheit_celcius-ff60a05d27237b3d.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/RustExs/MyDocTutorial/farenheit_celcius/target/debug/deps/farenheit_celcius-ff60a05d27237b3d.rmeta: src/main.rs
+
+/data/data/com.termux/files/home/RustExs/MyDocTutorial/farenheit_celcius/target/debug/deps/farenheit_celcius-ff60a05d27237b3d.d: src/main.rs
+
+src/main.rs:
