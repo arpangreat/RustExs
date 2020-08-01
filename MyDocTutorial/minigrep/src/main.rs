662f70e4864c33e410d7f78caf7f09003e0de59c
arpangreat <arpanthegreat41@gmail.com>
2020-08-01 18:02:09 +0530

    minigrep project complete

--- MyDocTutorial/minigrep/src/main.rs
+++ MyDocTutorial/minigrep/src/main.rs
@@ -7,15 +7,16 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem Parsing Arguements: {}", err);
+        eprintln!("Problem Parsing Arguements: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    //    println!("Searching for {}", config.query);
+
+    //    println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
